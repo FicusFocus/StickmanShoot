72eb8dd8704da27afe8e4f1c74ddf926c98b91de
FicusFocus <shkurihin.pashok@mail.ru>
2022-03-08 02:39:40 +0600

    Finnish

---
+++ Assets/Skripts/AmmoPack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AmmoPack : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _ammoCoutText;
+    [SerializeField] private Transform _pack;
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private int _ammoInPack;
+
+    private Collider _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+        _collider.isTrigger = true;
+        _ammoCoutText.text = _ammoInPack.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Chamber chamber))
+        {
+            chamber.TakeAmmo(_ammoInPack);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Skripts/GunLevelUper.cs
+++ Assets/Skripts/GunLevelUper.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
@@ -6,12 +7,35 @@ public class GunLevelUper : MonoBehaviour
     [Range (0, 1)][SerializeField] private float _fireRateUpInPersent = 0.2f;
     [SerializeField] private Gun _gun;
 
+    private float _destroyingScale;
+    private Vector3 _increaseStep = new Vector3(0.1f, 0.1f, 0.1f);
+    private Vector3 _dyingTargetScale;
+    private IEnumerator _destroyedProcess;
+
+    private void Start()
+    {
+        _dyingTargetScale = transform.localScale * 2;
+        _destroyingScale = _dyingTargetScale.x;
+        _destroyedProcess = Destoying();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Player player))
         {
             _gun.UpFireRate(_fireRateUpInPersent);
-            Destroy(gameObject);
+            StartCoroutine(_destroyedProcess);
         }
+    }
+
+    private IEnumerator Destoying()
+    {
+        while (transform.localScale.x <= _destroyingScale)
+        {
+            transform.localScale += _increaseStep;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
